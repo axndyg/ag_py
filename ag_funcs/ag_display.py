4c522142505d270f4879c0ac5589a66c046d72db
axndyg <andyg.2003@yahoo.com>
2024-08-14 11:52:43 -0700

    Add base display tool

---
+++ ag_funcs/ag_display.py
@@ -0,0 +1,11 @@
+from IPython.display import clear_output
+
+def display_progress(ind, tot, limiter): 
+    if ind % limiter == 0: 
+        bar = ["-" for spc in range(10)]
+        fill = int(ind / tot * 10) 
+        if fill > 0: 
+            for f in range(fill): 
+                bar[f] = "x"
+        clear_output(wait=True)
+        print("[" + "".join(bar) + "]")
